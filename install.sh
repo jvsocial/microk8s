d4c53113202f436421fbde2a8934a9594274ab5b
jvsocial <78414554+jvsocial@users.noreply.github.com>
2024-10-26 10:55:40 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 sudo snap install microk8s --classic --channel=1.31
-sudo usermod -a -G microk8s $USER
+sudo usermod -a -G microk8s ubuntu
 mkdir -p ~/.kube
 chmod 0700 ~/.kube
+chown -R ubuntu ~/.kube
